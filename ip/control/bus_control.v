8c9f96ede98a22fce788d0f016f9aa0b7f709861
Anderson Hsieh <andy13579246810@gmail.com>
2023-08-29 23:34:35 -0400

    start working on bus control

--- ip/control/bus_control.v
+++ ip/control/bus_control.v
@@ -1,13 +1,62 @@
 module bus_control 
 (
-	input [2:0] reg_en, // encoded, from instruction decoder
-	input [`ISA_INSTRUCTION_COUNT-1:0] instruction_en,
-	output [7:0] reg_read_en, 
-	output [7:0] reg_write_en
+	input clk,
+	input [2:0] reg_num, // encoded, from instruction decoder.
+	input [`ISA_INSTRUCTION_COUNT-1:0] instruction_en, // from instruction decoder
+
+	//--- for program counter register
+	output reg reg_pc_write_en,
+	output reg reg_pc_read_en,
+
+	//--- for register 0 ~ 8
+	output reg [7:0] reg_read_en,  
+	output reg [7:0] reg_write_en
 );
 
+reg [1:0] counter; // in my ISA the longest instruction takes 2 cycles so 2 bits
+reg instruction_done; // when current clk cycle is the last cycle of current executing instruction, this signal goes HIGH
+
 always @(*) begin
+	//Defaults
 	
+	reg_pc_write_en <= 1'b0;
+	reg_pc_read_en <= 1'b0;
+	reg_read_en <= 8'b0;
+	reg_write_en <= 8'b0;
+	
+	instruction_done <= 1'b0;
+	counter <= 2'b0;
+
+	if (instruction_en[`ISA_ADD] |
+		insturction_en[`ISA_SH]  |
+		insturction_en[`ISA_AND] |
+		instruction_en[`ISA_OR]  |
+		instruction_en[`ISA_XOR] |
+		instruction_en[`]) begin
+		reg_read_en[reg_num] <= 1'b1;
+		reg_write_en[0] <= 1'b1;
+		instruction_done <= 1'b1;
+	end	
+
+	if (instruction_en[`ISA_CPY] |
+		instruction_en[`ISA_LB] |
+		instruction_en[]) begin
+		reg_read_en[0] <= 1'b1;
+		reg_write_en[reg_num] <= 1'b1;
+	end
+
+	if (instruction_en[`ISA_CPYPC]) begin
+		reg_pc_read_en <= 1'b1;
+		reg_write_en[reg_num] <= 1'b1;
+	end
+
+	if (instruction_en[`ISA_ADDI] | 
+		instruction_en[`ISA_SHI]  | 
+		instruction_en[`ISA_NOT]  |
+	) begin
+		reg_write_en[0] <= 1'b1;
+	end
+
 end
 
 endmodule
--- ip/control/instruction_decoder.v
+++ ip/control/instruction_decoder.v
@@ -16,68 +16,68 @@ assign reg_bits = instruction[2:0];
 
 always @(*) begin
 	// Defaults
-	reg_en = reg_bits;
-	imm = 4'b000;
-	instruction_en = {`ISA_INSTRUCTION_COUNT{1'b0}};
+	reg_en <= reg_bits;
+	imm <= 4'b000;
+	instruction_en <= {`ISA_INSTRUCTION_COUNT{1'b0}};
 
 	case (opcode)
 		4'b0000: begin
-			instruction_en[`ISA_ADD] = 1'b1;
+			instruction_en[`ISA_ADD] <= 1'b1;
 		end
 		4'b0001: begin
-			instruction_en[`ISA_ADDI] = 1'b1;
-			imm = {utility_bit, reg_bits};
+			instruction_en[`ISA_ADDI] <= 1'b1;
+			imm <= {utility_bit, reg_bits};
 		end
 		4'b0010: begin
-			instruction_en[`ISA_SH] = 1'b1;
+			instruction_en[`ISA_SH] <= 1'b1;
 		end
 		4'b0011: begin
-			instruction_en[`ISA_SHI] = 1'b1;
-			imm = {utility_bit, reg_bits};
+			instruction_en[`ISA_SHI] <= 1'b1;
+			imm <= {utility_bit, reg_bits};
 		end
 		4'b0100: begin
-			instruction_en[`ISA_NOT] = 1'b1;
+			instruction_en[`ISA_NOT] <= 1'b1;
 		end
 		4'b0101: begin
-			instruction_en[`ISA_AND] = 1'b1;
+			instruction_en[`ISA_AND] <= 1'b1;
 		end
 		4'b0110: begin
-			instruction_en[`ISA_OR] = 1'b1;
+			instruction_en[`ISA_OR] <= 1'b1;
 		end
 		4'b0111: begin
-			instruction_en[`ISA_XOR] = 1'b1;
+			instruction_en[`ISA_XOR] <= 1'b1;
 		end
 		4'b1000: begin
 			if (utility_bit) begin
-				instruction_en[`ISA_CPYPC] = 1'b1;
+				instruction_en[`ISA_CPYPC] <= 1'b1;
 			end else begin
-				instruction_en[`ISA_CPY] = 1'b1;
+				instruction_en[`ISA_CPY] <= 1'b1;
 			end
 		end
 		4'b1001: begin
-			instruction_en[`ISA_LB] = 1'b1;
+			instruction_en[`ISA_LB] <= 1'b1;
 		end
 		4'b1010: begin
-			instruction_en[`ISA_SB] = 1'b1;
+			instruction_en[`ISA_SB] <= 1'b1;
 		end
 		4'b1011: begin
-			instruction_en[`ISA_JMPADR] = 1'b1;
+			instruction_en[`ISA_JMPADR] <= 1'b1;
 		end
 		4'b1100: begin
-			instruction_en[`ISA_JMPI] = 1'b1;
-			imm = {utility_bit, reg_bits};
+			instruction_en[`ISA_JMPI] <= 1'b1;
+			imm <= {utility_bit, reg_bits};
 		end
 		4'b1101: begin
-			instruction_en[`ISA_BLT] = 1'b1;
+			instruction_en[`ISA_BLT] <= 1'b1;
 		end
 		4'b1110: begin
-			instruction_en[`ISA_BGT] = 1'b1;
+			instruction_en[`ISA_BGT] <= 1'b1;
 		end
 		4'b1111: begin
 			if (utility_bit) begin
-				instruction_en[`ISA_BNEQ] = 1'b1;
+				instruction_en[`ISA_BNEQ] <= 1'b1;
 			end else begin
-				instruction_en[`ISA_BEQ] = 1'b1;
+				instruction_en[`ISA_BEQ] <= 1'b1;
 			end
 		end
 		default: // do nothing
